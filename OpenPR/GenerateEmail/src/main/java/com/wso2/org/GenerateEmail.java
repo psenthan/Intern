91e8471c81612873c97b6f8211856adbddcf8766
psenthan <senthanprasanth007@gmail.com>
2017-12-19 19:04:43 +0530

    Generate Email

---
+++ OpenPR/GenerateEmail/src/main/java/com/wso2/org/GenerateEmail.java
@@ -0,0 +1,45 @@
+package com.wso2.org;
+
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class GenerateEmail
+{
+    public static void main( String[] args )
+    {
+        final String senderEmail="senthanprasanth007@gmail.com";
+        final String password="dic0112365831";
+        Properties props =new Properties();
+        props.put("mail.smtp.auth","true");
+        props.put("mail.smtp.starttls.enable","true");
+        props.put("mail.smtp.host","smtp.gmail.com");
+        props.put("mail.smtp.port","587");
+
+        Session session=Session.getInstance(props,new javax.mail.Authenticator(){
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(senderEmail,password);
+            }
+        });
+
+        try{
+            Message message=new MimeMessage(session);
+            message.setFrom(new InternetAddress("senthan@wso2.com"));
+            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("senthan@wso2.com"));
+            message.setSubject("Hello message");
+            message.setText("Hello prasanth!!!!!!!");
+
+            Transport.send(message);
+
+            System.out.println("done");
+
+        }
+        catch (MessagingException r){
+            throw new RuntimeException(r);
+        }
+
+
+    }
+}
---
+++ OpenPR/GenerateEmail/src/main/java/com/wso2/org/OpenPRpdfReport.java
@@ -0,0 +1,70 @@
+package com.wso2.org;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
+
+import java.io.FileOutputStream;
+import java.sql.*;
+
+
+public class OpenPRpdfReport {
+    public static void main(String[] args) throws Exception{
+
+
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/open-pr-requests", "root", "wso2123");
+        Statement stmt = conn.createStatement();
+
+                /* Define the SQL query */
+
+        ResultSet openPr = stmt.executeQuery("SELECT distinct Product,count(PullUrl) as countPR FROM RetrieveOpenPR where OpenWeeks>1 group by Product having countPR order by countPR desc;");
+
+                /*  Initialize PDF documents - logical objects */
+        Document openPRreport = new Document();
+        PdfWriter.getInstance(openPRreport, new FileOutputStream("OpenPRreport.pdf"));
+        openPRreport.open();
+        openPRreport.addTitle("Number of Open PR more than a week");
+
+
+        //Define number if columns
+        PdfPTable openPRtable = new PdfPTable(2);
+
+
+        //create a cell object
+        PdfPCell tablecell;
+
+
+
+
+        while (openPr.next()) {
+
+            String productName = openPr.getString("Product");
+            tablecell=new PdfPCell(new Phrase(productName));
+            openPRtable.addCell(tablecell);
+
+            String countPRMoreWeek=Integer.toString(Integer.parseInt(openPr.getString("countPR")));
+            tablecell=new PdfPCell(new Phrase(countPRMoreWeek));
+            openPRtable.addCell(tablecell);
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+                /* Attach report table to PDF */
+        openPRreport.add(openPRtable);
+        openPRreport.close();
+
+                /* Close all DB related objects */
+        openPr.close();
+        stmt.close();
+        conn.close();
+
+    }
+}
